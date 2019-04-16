4ea848cd009a9ed573777a81d4a6fd02cd111713
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-04-16 16:32:03 -0300

    Bio List and inline css

---
+++ src/app/dataTypeObjects/bioList.ts
@@ -0,0 +1,21 @@
+export class BioList {
+    auditToken: string;
+    conditions: ConditionsBioList = new ConditionsBioList();
+}
+
+export class ConditionsBioList {
+    i1: number;
+    i2: number;
+    i3: number;
+    i4: number;
+    i5: number;
+    id: string;
+    idIn: string;
+    idLike: string;
+    idNo: string;
+    s1: string;
+    s2: string;
+    s3: string;
+    s4: string;
+    s5: string;
+}
