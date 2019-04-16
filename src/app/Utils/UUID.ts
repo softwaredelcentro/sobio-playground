a5808d3bce919f5648d2526dd7f4e74105f65c05
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-04-16 16:30:17 -0300

    UUID Util

---
+++ src/app/Utils/UUID.ts
@@ -0,0 +1,17 @@
+export class UUID {
+
+    static create_UUID(tpl?: string): string {
+        if (!tpl) {
+            tpl = 'xxxxxxxx';
+        }
+        let dt = new Date().getTime();
+        const uuid = tpl.replace(/[xy]/g, (c) => {
+            // tslint:disable-next-line:no-bitwise
+            const r = (dt + Math.random() * 16) % 16 | 0;
+            dt = Math.floor(dt / 16);
+            // tslint:disable-next-line:no-bitwise
+            return (c === 'x' ? r : (r & 0x3 | 0x8) ).toString(16);
+        });
+        return uuid;
+    }
+}
