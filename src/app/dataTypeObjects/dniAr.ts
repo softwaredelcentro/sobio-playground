313212d14f6e57eb8d85ac08898a82852defef35
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-04-22 14:13:10 -0300

    new dni params.

--- src/app/dataTypeObjects/dniAr.ts
+++ src/app/dataTypeObjects/dniAr.ts
@@ -2,4 +2,14 @@ export class DniAr {
     auditToken: string;
     backImage: string;
     frontImage: string;
+    extractParams: DniArParameters = new DniArParameters();
+}
+
+export class DniArParameters {
+
+    public extractPDF417 = true;
+    public extractMRZ = true;
+    public extractFace = true;
+    public useCache = true;
+
 }
