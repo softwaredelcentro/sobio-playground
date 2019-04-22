1edc144152987de415d55863f23455d1dc84e924
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-04-22 14:16:51 -0300

    fixing param name: extractParams -> params

--- src/app/dataTypeObjects/dniAr.ts
+++ src/app/dataTypeObjects/dniAr.ts
@@ -2,7 +2,7 @@ export class DniAr {
     auditToken: string;
     backImage: string;
     frontImage: string;
-    extractParams: DniArParameters = new DniArParameters();
+    params: DniArParameters = new DniArParameters();
 }
 
 export class DniArParameters {
