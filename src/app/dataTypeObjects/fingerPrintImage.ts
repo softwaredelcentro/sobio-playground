877124fdc8af93674ee51afe699b0853865b0abe
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-04-22 16:21:30 -0300

    Finger print form.

--- src/app/dataTypeObjects/fingerPrintImage.ts
+++ src/app/dataTypeObjects/fingerPrintImage.ts
@@ -8,8 +8,8 @@ export class FingerPrintParams {
     public determinePatternClass = true;
     public evaluateNFIQ = true;
     public imageSource = 'LIVE_FINGERPRINT';
-    public maxRotation = 0;
-    public minQuality = 0;
+    public maxRotation = 45;
+    public minQuality = 10;
     public position = 'RIGHT_THUMB';
     public useCache = true;
     public useOnlyAnsi378 = true;
--- src/app/pages/extraction/extract-fingerprint-image/extract-fingerprint-image.component.ts
+++ src/app/pages/extraction/extract-fingerprint-image/extract-fingerprint-image.component.ts
@@ -1,17 +1,31 @@
+import { FingerPrintImage } from './../../../dataTypeObjects/fingerPrintImage';
 import { Component, OnInit } from '@angular/core';
+import { UUID } from 'src/app/Utils/UUID';
 
 @Component({
   selector: 'app-extract-fingerprint-image',
   templateUrl: './extract-fingerprint-image.component.html',
-  styleUrls: ['./extract-fingerprint-image.component.sass']
+  styleUrls: [
+    './extract-fingerprint-image.component.sass',
+    '../../../shared-styles/forms.scss',
+    '../../../shared-styles/playground-response.scss'
+  ]
 })
 export class ExtractFingerprintImageComponent implements OnInit {
 
   loading: boolean;
+  step: number;
+  dataType: string;
+  dto: FingerPrintImage;
 
-  constructor() { }
+  constructor() {
+    this.dto = new FingerPrintImage();
+    this.dto.auditToken = UUID.create();
+  }
 
   ngOnInit() {
+    this.step = 1;
+    this.loading = false;
   }
 
 }
